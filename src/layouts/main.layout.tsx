3e76192794f8e14e53dbc4fc573e199fe9241bca
Andrej Stevkovski <stevandrej@gmail.com>
2025-01-02 23:29:51 +0100

    git file case sensitivity -false

--- src/layouts/main.layout.tsx
+++ src/layouts/main.layout.tsx
@@ -3,30 +3,30 @@ import { Sidebar } from "../features/layout-sidebar/sidebar";
 import { cn } from "../lib/utils";
 
 interface MainLayoutProps {
-  children: ReactNode;
+	children: ReactNode;
 }
 
 export const MainLayout = ({ children }: MainLayoutProps) => {
-  const [open, setOpen] = useState(false);
+	const [open, setOpen] = useState(false);
 
-  return (
-    <>
-      <div
-        className={cn(
-          "flex flex-col md:flex-row bg-slate-300 w-full h-screen flex-1 mx-auto"
-        )}
-      >
-        <Sidebar open={open} setOpen={setOpen} animate />
-        <div
-          className={cn(
-            "transition-all duration-[2000ms] ease-in-out",
-            "p-2 md:p-10 flex flex-col gap-2 flex-1 w-full h-full md:ml-[60px] overflow-auto",
-            "bg-white"
-          )}
-        >
-          {children}
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div
+				className={cn(
+					"flex flex-col md:flex-row bg-slate-300 w-full h-screen flex-1 mx-auto"
+				)}
+			>
+				<Sidebar open={open} setOpen={setOpen} animate />
+				<div
+					className={cn(
+						"transition-all duration-[2000ms] ease-in-out",
+						"p-2 md:p-10 flex flex-col gap-2 flex-1 w-full h-full md:ml-[60px] overflow-auto",
+						"bg-white"
+					)}
+				>
+					{children}
+				</div>
+			</div>
+		</>
+	);
 };
